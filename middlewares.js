cb30fa0137ae96cc57e2bd0d49f0118407ee7019
Paulo Pires <taralloqueiroiz@gmail.com>
2015-06-27 19:00:36 -0300

    more arrow functions

--- middlewares.js
+++ middlewares.js
@@ -5,20 +5,20 @@ let Bootstrap = React.createFactory(BootstrapFactory);
 export default (app) => {
 
   // Bootstrap the App
-  app.get('/', function(req, res){
+  app.get('/', (req, res) => {
 		let reactHtml = React.renderToString(Bootstrap({}));
     res.render('index.ejs', {reactOutput: reactHtml});
 	});
 
   // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
+  app.use((req, res, next) => {
     let err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
 
   // error handlers
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
