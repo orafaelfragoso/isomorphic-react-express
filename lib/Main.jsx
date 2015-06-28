0f3d8e79ff9951faedd4ea79f28d5b0d0041076e
Rafael Fragoso <rafaelfragosom@gmail.com>
2015-06-28 16:20:05 +0100

    Added a package.json to use/manage its own dependencies

--- lib/Main.jsx
+++ lib/Main.jsx
@@ -1,4 +1,4 @@
 import React from 'react/addons';
-import Bootstrap from './Bootstrap/Bootstrap';
+import Bootstrap from 'Bootstrap';
 
 React.render(<Bootstrap />, document.getElementById('react-app'));
